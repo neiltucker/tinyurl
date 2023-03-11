855e9a9b0b15b45e02bae343390079d2e595dea9
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2023-03-11 01:49:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import gradio as gr
 import pyshorteners
+import pymssql 
+
+
+
+def dbupdate(longname,shortname):
+    dbconnect = pymssql.connect(server='nrtsqlserver1.database.windows.net', user='sqllogin1', password='Password1', database='nrtsql1')
+    cursordb = dbconnect.cursor()
+    cursordb.execute("""INSERT INTO webaddress (longurl, shorturl) VALUES (%s, %s)""", (longname, shortname))
+    cursordb.close()
+    dbconnect.commit()
+    dbconnect.close()
+
+
 
 def shortenurl(name):
+    longname = name
     shortener = pyshorteners.Shortener()
-    return (shortener.tinyurl.short(name))
-
+    shortname = shortener.tinyurl.short(longname)
+    dbupdate(longname,shortname)
+    return (shortname)
 
 
 
 app = gr.Interface(fn=shortenurl, inputs="text", outputs="text")
-
-app.launch(server_name="0.0.0.0", server_port=80)   
+app.launch(server_name="0.0.0.0", server_port=80) 
